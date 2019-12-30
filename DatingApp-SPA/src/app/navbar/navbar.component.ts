2f6d117dfef4403b9360ea3ccadb38a7cc5d92a6
Andrey Penin <penin.andrew@yandex.ru>
2019-12-30 11:58:54 +0400

    End od Section 4

---
+++ DatingApp-SPA/src/app/navbar/navbar.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { tokenName } from '@angular/compiler';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  model: any = {};
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in succesfull');
+    }, error => {
+      console.log('Failed to login');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+
+}
